bb2958ea144e6bb6d354ef885109d668698f5b56
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2025-03-17 20:30:53 +0900

    feat: add server side rendering via cloudflare workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: "server",
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   },
+
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  site: "https://indiehackers.tokyo",
 });
